1399df723dc92d689efee25f3ecc93628f718ab5
Pavel Lobashov <shockwavenn@gmail.com>
2017-10-13 16:44:18 +0300

    Add codecov support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+if ENV['CI'] == 'true'
+  require 'simplecov'
+  SimpleCov.start
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'bundler/setup'
 require 'onlyoffice_language_helper'
 
