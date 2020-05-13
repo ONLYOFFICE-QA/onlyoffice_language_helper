235874005235bda50f4a6696c34f0893b5d18ecf
Pavel Lobashov <shockwavenn@gmail.com>
2020-05-13 18:07:36 +0300

    Fix rubocop `RSpec/ExampleLength` cop

--- spec/onlyoffice_language_helper/spell_checker_incorret_dic_spec.rb
+++ spec/onlyoffice_language_helper/spell_checker_incorret_dic_spec.rb
@@ -3,15 +3,16 @@
 require 'spec_helper'
 
 RSpec.describe OnlyofficeLanguageHelper::SpellChecker do
+  let(:incorrect_path) { '/tmp' }
+
   after do
     described_class.reset_config
   end
 
   it '#configure dictionaries_path' do
-    dictionaries_path = '/tmp'
     expect do
       described_class.configure do |config|
-        config.dictionaries_path = dictionaries_path
+        config.dictionaries_path = incorrect_path
       end
     end.to raise_error(RuntimeError)
   end
